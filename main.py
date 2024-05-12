beecac698c0f4a59d30febcec255d503e5adb1f7
space-santa <space-santa@outlook.com>
2024-05-12 19:57:55 +1000

    Get colour from API

--- main.py
+++ main.py
@@ -8,33 +8,45 @@ The sense hat displays the appropriate colour for the season.
 import time
 
 import pygame
+import requests
 from sense_hat import SenseHat
 
 sense = SenseHat()
 
-green = (0, 128, 43)
-purple = (179, 0, 179)
-red = (255, 0, 0)
-white = (255, 255, 255)
-pink = (255, 102, 255)
+COLOURS = {
+    "green": (0, 128, 43),
+    "purple": (179, 0, 179),
+    "red": (255, 0, 0),
+    "white": (255, 255, 255),
+    "pink": (255, 102, 255),
+    "brown": (204, 153, 0),
+}
 
-brown = (204, 153, 0)
 
-bg = pink
-cr = brown
+def get_colour_name(date_string: str):
+    response = requests.get(
+        "https://sharp.clau.space/api/colour-of-the-day",
+        data={"date": date_string},
+        timeout=10,
+    )
+    return response.json()["colour"]
 
-# fmt:off
-pixels = [
-    bg,bg,bg,cr,cr,bg,bg,bg,
-    bg,bg,bg,cr,cr,bg,bg,bg,
-    cr,cr,cr,cr,cr,cr,cr,cr,
-    cr,cr,cr,cr,cr,cr,cr,cr,
-    bg,bg,bg,cr,cr,bg,bg,bg,
-    bg,bg,bg,cr,cr,bg,bg,bg,
-    bg,bg,bg,cr,cr,bg,bg,bg,
-    bg,bg,bg,cr,cr,bg,bg,bg,
-]
-# fmt:on
+
+def get_pixelmap(colour_name):
+    cr = COLOURS["brown"]
+    bg = COLOURS[colour_name]
+    # fmt:off
+    return [
+        bg, bg, bg, cr, cr, bg, bg, bg,
+        bg, bg, bg, cr, cr, bg, bg, bg,
+        cr, cr, cr, cr, cr, cr, cr, cr,
+        cr, cr, cr, cr, cr, cr, cr, cr,
+        bg, bg, bg, cr, cr, bg, bg, bg,
+        bg, bg, bg, cr, cr, bg, bg, bg,
+        bg, bg, bg, cr, cr, bg, bg, bg,
+        bg, bg, bg, cr, cr, bg, bg, bg,
+    ]
+    # fmt:on
 
 
 def ring_bell():
@@ -48,6 +60,11 @@ def main():
     minutes = current_time.tm_min
     hour = current_time.tm_hour
 
+    colour_name = get_colour_name(
+        f"{current_time.tm_year}-{current_time.tm_mon}-{current_time.tm_mday}"
+    )
+    pixels = get_pixelmap(colour_name)
+
     hour_times = 0
 
     if minutes == 15:
@@ -57,7 +74,7 @@ def main():
     elif minutes == 45:
         times = 3
     elif minutes == 0:
-        times = 4 + hour  # Play additional sounds according to the hour
+        times = 4
         hour_times = hour
     else:
         times = 1
