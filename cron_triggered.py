c78f093e17fd26fae66398d2a22e9c010ce93f7f
Claus Zirkel <spacesanta@outlook.com>
2024-11-02 20:51:44 +1100

    Lower volumne

--- cron_triggered.py
+++ cron_triggered.py
@@ -6,7 +6,7 @@ times, hour_times = determine_rings()
 # times = 5
 response = requests.get(
     "http://localhost:8456/api/ring/",
-    params={"times": times, "hour_times": hour_times, "volume": 0.2},
+    params={"times": times, "hour_times": hour_times, "volume": 0.1},
     timeout=30,
 )
 
